a04e2d4ce54a3f5549df05e37f78ad075d531f33
teru <daiki.teruya@usp.br>
2026-02-16 23:19:29 -0300

    tweaking db

--- db/db.go
+++ db/db.go
@@ -1,3 +1,4 @@
+// Package db contains database code
 package db
 
 import (
@@ -6,33 +7,31 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
-type DBManager struct {
-	db *sql.DB
-}
-
 type User struct {
 	ID     int32
 	Name   string
 	Passwd string
 }
 
+type DBManager struct {
+	db *sql.DB
+}
+
 func (dbm *DBManager) Connect(user string, passwd string, dbname string) error {
+	var err error
+
 	cfg := mysql.NewConfig()
 	cfg.User = user
 	cfg.Passwd = passwd
 	cfg.DBName = dbname
 
-	var err error
 	dbm.db, err = sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
 
 func (dbm *DBManager) CreateTables(drop bool) error {
 	var err error
+
 	if drop {
 		_, err = dbm.db.Exec("DROP TABLE IF EXISTS users")
 		if err != nil {
@@ -50,21 +49,21 @@ func (dbm *DBManager) CreateTables(drop bool) error {
 	return err
 }
 
-func (dbm *DBManager) GetUserFromID(id int32) (*User, error) {
-	var err error
+func (dbm *DBManager) GetUserFromID(id int32) (user *User, err error) {
 	q, err := dbm.db.Query("SELECT * FROM users WHERE id = ?", id)
 	if err != nil {
 		return nil, err
 	}
-	defer q.Close()
+	defer func() {
+		err = q.Close()
+	}()
 
 	if !q.Next() {
 		return nil, q.Err()
 	}
-	var user User
 	err = q.Scan(&user.ID, &user.Name, &user.Passwd)
 	if err != nil {
 		return nil, err
 	}
-	return &user, nil
+	return user, nil
 }
